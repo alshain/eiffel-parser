a359e6cf15ddd8d478d0347289251d9030ff6b6e
Christian Vonrüti <christian@vonrueti.ch>
2015-03-16 11:49:15 +0100

    Fix any builtin: field -> attr

--- src/eiffel/any.js
+++ src/eiffel/any.js
@@ -3,7 +3,7 @@ root.vees.builtin.classes.push(function(c, a, p, f) {
   c("ANY", null,
     // Fields
     [
-      f("Io", "STD_FILES")
+      a("Io", "STD_FILES"),
     ],
     // Functions
     {  },
