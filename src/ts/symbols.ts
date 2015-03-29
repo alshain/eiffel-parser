ddc106e6ef419f215094295a5ce04847888fb0a2
Christian Vonrüti <christian@vonrueti.ch>
2015-03-29 15:44:00 +0200

    Reindent ClassSymbol

--- src/ts/symbols.ts
+++ src/ts/symbols.ts
@@ -47,40 +47,40 @@ module eiffel.symbols {
     ast: ast.VarOrConstAttribute;
   }
 
-    export class ClassSymbol extends Symbol {
-      constructor(name:string, ast: ast.Class) {
-        super(name);
-        this.ast = ast;
-      }
-
-      ast: ast.Class;
-      functions: LookupTable<FunctionSymbol> = {};
-      procedures: LookupTable<ProcedureSymbol> = {};
-      routines: LookupTable<RoutineSymbol> = {};
-      attributes: LookupTable<AttributeSymbol> = {};
-
-
-      hasSymbol(name: string): boolean {
-        if (this.routines.hasOwnProperty(name)) {
-          return true;
-        }
-        if (this.attributes.hasOwnProperty(name)) {
-          return true;
-        }
-        return false;
-      }
-
-      resolveSymbol(name: string): Symbol {
-        if (this.routines.hasOwnProperty(name)) {
-          return this.functions[name];
-        }
-        if (this.procedures.hasOwnProperty(name)) {
-          return this.procedures[name];
-        }
-        if (this.attributes.hasOwnProperty(name)) {
-          return this.attributes[name];
-        }
-        throw new Error("Symbol " + name + " does not exist in class " + this.name + ".");
-      }
+  export class ClassSymbol extends Symbol {
+    constructor(name:string, ast: ast.Class) {
+      super(name);
+      this.ast = ast;
     }
+
+    ast: ast.Class;
+    functions: LookupTable<FunctionSymbol> = {};
+    procedures: LookupTable<ProcedureSymbol> = {};
+    routines: LookupTable<RoutineSymbol> = {};
+    attributes: LookupTable<AttributeSymbol> = {};
+
+
+    hasSymbol(name: string): boolean {
+      if (this.routines.hasOwnProperty(name)) {
+        return true;
+      }
+      if (this.attributes.hasOwnProperty(name)) {
+        return true;
+      }
+      return false;
+    }
+
+    resolveSymbol(name: string): Symbol {
+      if (this.routines.hasOwnProperty(name)) {
+        return this.functions[name];
+      }
+      if (this.procedures.hasOwnProperty(name)) {
+        return this.procedures[name];
+      }
+      if (this.attributes.hasOwnProperty(name)) {
+        return this.attributes[name];
+      }
+      throw new Error("Symbol " + name + " does not exist in class " + this.name + ".");
+    }
+  }
 }
