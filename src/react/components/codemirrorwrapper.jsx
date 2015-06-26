f1f2fc4db5bb7f24f1533c4f3e8b79ca54f4d398
Christian Vonrüti <christian@vonrueti.ch>
2015-06-26 21:49:20 +0200

    No need to require eiffel mode here

--- src/react/components/codemirrorwrapper.jsx
+++ src/react/components/codemirrorwrapper.jsx
@@ -5,9 +5,6 @@ var FileDragAndDrop = require('react-file-drag-and-drop');
 var Codemirror = require('react-codemirror');
 let {Paper} = mui;
 
-require('../../../../node_modules/react-codemirror/node_modules/codemirror/mode/eiffel/eiffel');
-
-
 var Wrapper = React.createClass({
   shouldComponentUpdate: function() {
     return false;
